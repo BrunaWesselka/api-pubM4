b7bc6f0a6f292324d2d9369e3129737f95ca3fc3
GlaucoTolotto <glaucotolotto@gmail.com>
2021-07-21 18:10:06 -0300

    Pasta models criada; database retirado;

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-barser')
 const app = express();
 const {APP_PORT} = require('./config/config.js');
 
-
+app.use(bodyParser.json());
 
 app.listen(APP_PORT, console.log("Servidor funcionando na porta 3000"));
