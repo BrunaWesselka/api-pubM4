1473d791ec71449cbbf3c337b47c898462e89353
jrbert <cabfjunior@gmail.com>
2021-07-23 19:22:26 -0300

    Tabela bebidas quentes finalizada

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser());
 app.use(express.json());
+app.use('/', roteador);
 const {APP_PORT} = require('./config/config.js');
 
-app.use('/', roteador);
+
 
 //pedindo para porta que esta no arquivo de config
 app.listen(APP_PORT, console.log("Servidor funcionando na porta 3000"));
