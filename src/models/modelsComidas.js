c150e314497a9cce6a380554417ed1cbe21e6df8
Pedro Ronald <pedrobarbosa.developer@gmail.com>
2021-07-23 21:39:22 -0300

    tabela comidas

--- src/models/modelsComidas.js
+++ src/models/modelsComidas.js
@@ -1,24 +1,39 @@
 const {Sequelize, DataTypes} = require('sequelize');
-const database = require('../database/data.js');
+ const database = require('../database/data.js');
 
-const Comidas = database.define('Comidas', {
+// Criando a tabela e definindo as colunas da tabela de Comidas
+const Comidas = database.define('Comidas',{
     ID: {
         type: DataTypes.UUID,
         defaultValue: Sequelize.UUIDV4,
         primaryKey: true
     },
-    Tipo_de_comida:{
+
+    OPCAO_DE_COMIDA: {
         type: DataTypes.STRING(20),
         allowNull: false
     },
-    Preco:{
-        type: DataTypes.NUMBER(6),
+
+    TIPO_DE_COMIDA: {
+        type: DataTypes.STRING(20),
+        allowNull: false
+    },
+    
+    SOBREMESA:{
+        type: DataTypes.STRING(20),
+        allowNull: false
+    },
+
+    PRECO: {
+        type: DataTypes.NUMBER(10),
         allowNull: false
     }
 })
-async function criarTabelaComidas (){
+
+
+async function criaTabelaDeComidas(){
     await Comidas.sync()
 }
 
-criarTabelaComidas ();
+criaTabelaDeComidas();
 module.exports = Comidas;
