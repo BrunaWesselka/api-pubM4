ff09382c6aee5bcd27d0a0acf1eb990b1cf31c17
Pedro Ronald <pedrobarbosa.developer@gmail.com>
2021-07-23 22:00:22 -0300

    rotas comida

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,11 +4,10 @@ const roteador = express.Router();
 const ControllerMusicas = require('../controller/musicasController.js');
 
 //CRUD da rota de musicas
-roteador.get('/musicas',ControllerMusicas.index);
-roteador.get('/muscias',ControllerMusicas.show);
-roteador.post('/musicas',ControllerMusicas.store);
-roteador.patch('/musicas',ControllerMusicas.update);
+roteador.get('/musicas', ControllerMusicas.index);
+roteador.post('/musicas', ControllerMusicas.store);
 roteador.delete('/musicas',ControllerMusicas.destroy);
+roteador.patch('/musicas', ControllerMusicas.update);
 
 
 const ControllerBebidasQuentes = require('../controller/bebidasquentesController.js');
@@ -17,7 +16,18 @@ const ControllerBebidasQuentes = require('../controller/bebidasquentesController
 roteador.get('/bebidasquentes',ControllerBebidasQuentes.index);
 roteador.get('/bebidasquentes',ControllerBebidasQuentes.show);
 roteador.post('/bebidasquentes',ControllerBebidasQuentes.store);
-roteador.patch('/bebidasquentes',ControllerBebidasQuentes.update);
-roteador.delete('/bebidasquentes',ControllerBebidasQuentes.destroy);
+roteador.patch('/bebidasquentes', ControllerBebidasQuentes.update);
+roteador.delete('/bebidasquentes', ControllerBebidasQuentes.destroy);
+
+module.exports = roteador;
+
+const ControllerBebidas = require('../controller/bebidasController.js');
+
+//CRUD da rota das Bebidas Frias
+roteador.get('/bebidas',ControllerBebidas.index);
+roteador.get('/bebidas',ControllerBebidas.show);
+roteador.post('/bebidas',ControllerBebidas.store);
+roteador.patch('/bebidas', ControllerBebidas.update);
+roteador.delete('/bebidas', ControllerBebidas.destroy);
 
 module.exports = roteador;
